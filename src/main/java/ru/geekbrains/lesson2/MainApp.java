7ff4865a57856eedbdc4073712edb15f44656962
Prokl <s.g.isakov@gmail.com>
2020-12-03 15:37:20 +0300

    task #1

--- src/main/java/ru/geekbrains/lesson2/MainApp.java
+++ src/main/java/ru/geekbrains/lesson2/MainApp.java
@@ -1,2 +1,18 @@
-package ru.geekbrains.lesson2;public class MainApp {
+package ru.geekbrains.lesson2;
+
+//Created by Sergey Isakov 03.12.2020
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("some words");
+        int a = 2;
+        int k;
+        int fin = 15;
+        int step = 2;
+        while (a < fin) {
+            a = a + step;
+            System.out.print(a + " ");
+        }
+
+    }
 }
